8fb8113ee587a2b41ae7faeb0058a4e20a216e32
pengp <pengp@certusnet.com.cn>
2018-12-06 17:19:13 +0800

    first commit

---
+++ src/main/java/com/penglecode/fabric/chaincode/bankmaster/chaincode/LoggerExample.java
@@ -0,0 +1,14 @@
+package com.penglecode.fabric.chaincode.bankmaster.chaincode;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoggerExample {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(LoggerExample.class);
+	
+	public static void main(String[] args) {
+		LOGGER.info(">>> LoggerExample");
+	}
+
+}
